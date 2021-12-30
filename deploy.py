e6c413eda8b72cc9b426a67c7f715b74453adc6d
gozarrojas <gozarrojas@outlook.es>
2021-12-30 11:49:41 -0500

    artifact

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 file = "build/objeto.txt"
 os.mkdir('build')
 #Descargamos el artefacto, porque es diferente para cada compilación
-os.system("buildkite-agent artifact download build/objeto.txt build/")
+os.system('buildkite-agent artifact download "build/objeto.txt" build/')
 
 try:
    with open(file,'a') as f:
